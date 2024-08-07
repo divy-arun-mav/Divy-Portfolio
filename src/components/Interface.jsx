c8097e723d9fe646193af7a1639de8a37bba0888
Divy Mav <divymav5@gmail.com>
2024-08-07 13:17:57 +0530

    Adding all files

---
+++ src/components/Interface.jsx
@@ -0,0 +1,277 @@
+import { motion } from "framer-motion";
+
+const Section = (props) => {
+  const { children } = props;
+
+  return (
+    <motion.section
+      className={`
+  h-screen w-screen p-8 max-w-screen-2xl mx-auto
+  flex flex-col items-start justify-center
+  `}
+      initial={{
+        opacity: 0,
+        y: 50,
+      }}
+      whileInView={{
+        opacity: 1,
+        y: 0,
+        transition: {
+          duration: 1,
+          delay: 0.6,
+        },
+      }}
+    >
+      {children}
+    </motion.section>
+  );
+};
+
+export const Interface = () => {
+  return (
+    <div className="flex flex-col items-center w-screen">
+      <AboutSection />
+      <SkillsSection />
+      <Section>
+        <h1 className="text-5xl font-bold my-10">Projects</h1>
+        <div className="w-7/12 rounded-xl bg-white p-2 flex justify-center items-center flex-col">
+          <img className="w-full rounded-xl" src="https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1200,h_630/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/tsah7c9evnal289z5fig/IMG%20Worlds%20of%20Adventure%20Admission%20Ticket%20in%20Dubai%20-%20Klook.jpg" alt="" srcset="" />
+          <p>EduManager an app to manage entire university</p><br />
+          <p>Web Development</p>
+        </div>
+      </Section>
+      <ContactSection />
+    </div>
+  );
+};
+
+const AboutSection = () => {
+  return (
+    <Section>
+      <h1 className="text-6xl font-extrabold leading-snug">
+        Hi, I'm
+        <br />
+        <span className="bg-white px-1 italic">Divy Mav</span>
+      </h1>
+      <motion.p
+        className="text-lg text-gray-600 mt-4"
+        initial={{
+          opacity: 0,
+          y: 25,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 1,
+          delay: 1.5,
+        }}
+      >
+        I develop interactive websites
+        <br />
+        A passionate developer who is keen to learn new technology <br /> to create new products with
+        beautifull and engaging design
+      </motion.p>
+      <motion.button
+        className={`bg-indigo-600 text-white py-4 px-8 
+      rounded-lg font-bold text-lg mt-16`}
+        initial={{
+          opacity: 0,
+          y: 25,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 1,
+          delay: 2,
+        }}
+      >
+        Contact me
+      </motion.button>
+    </Section>
+  );
+};
+
+const skills = [
+  {
+    title: "Threejs / React Three Fiber",
+    level: 80,
+  },
+  {
+    title: "React / React Native",
+    level: 90,
+  },
+  {
+    title: "Nodejs",
+    level: 90,
+  },
+  {
+    title: "Typescript",
+    level: 60,
+  },
+  {
+    title: "3D Modeling",
+    level: 40,
+  },
+];
+const languages = [
+  {
+    title: "🇫🇷 French",
+    level: 100,
+  },
+  {
+    title: "🇺🇸 English",
+    level: 80,
+  },
+  {
+    title: "🇯🇵 Japanese",
+    level: 20,
+  },
+];
+
+const SkillsSection = () => {
+  return (
+    <Section>
+      <motion.div whileInView={"visible"}>
+        <h2 className="text-5xl font-bold">Skills</h2>
+        <div className=" mt-8 space-y-4">
+          {skills.map((skill, index) => (
+            <div className="w-64" key={index}>
+              <motion.h3
+                className="text-xl font-bold text-gray-800"
+                initial={{
+                  opacity: 0,
+                }}
+                variants={{
+                  visible: {
+                    opacity: 1,
+                    transition: {
+                      duration: 1,
+                      delay: 1 + index * 0.2,
+                    },
+                  },
+                }}
+              >
+                {skill.title}
+              </motion.h3>
+              <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
+                <motion.div
+                  className="h-full bg-indigo-500 rounded-full "
+                  style={{ width: `${skill.level}%` }}
+                  initial={{
+                    scaleX: 0,
+                    originX: 0,
+                  }}
+                  variants={{
+                    visible: {
+                      scaleX: 1,
+                      transition: {
+                        duration: 1,
+                        delay: 1 + index * 0.2,
+                      },
+                    },
+                  }}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+        <div>
+          <h2 className="text-5xl font-bold mt-10">Languages</h2>
+          <div className=" mt-8 space-y-4">
+            {languages.map((lng, index) => (
+              <div className="w-64" key={index}>
+                <motion.h3
+                  className="text-xl font-bold text-gray-800"
+                  initial={{
+                    opacity: 0,
+                  }}
+                  variants={{
+                    visible: {
+                      opacity: 1,
+                      transition: {
+                        duration: 1,
+                        delay: 2 + index * 0.2,
+                      },
+                    },
+                  }}
+                >
+                  {lng.title}
+                </motion.h3>
+                <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
+                  <motion.div
+                    className="h-full bg-indigo-500 rounded-full "
+                    style={{ width: `${lng.level}%` }}
+                    initial={{
+                      scaleX: 0,
+                      originX: 0,
+                    }}
+                    variants={{
+                      visible: {
+                        scaleX: 1,
+                        transition: {
+                          duration: 1,
+                          delay: 2 + index * 0.2,
+                        },
+                      },
+                    }}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </motion.div>
+    </Section>
+  );
+};
+
+const ContactSection = () => {
+  return (
+    <Section>
+      <h2 className="text-5xl font-bold">Contact me</h2>
+      <div className="mt-8 p-8 rounded-md bg-white w-96 max-w-full">
+        <form>
+          <label for="name" className="font-medium text-gray-900 block mb-1">
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+          />
+          <label
+            for="email"
+            className="font-medium text-gray-900 block mb-1 mt-8"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+          />
+          <label
+            for="email"
+            className="font-medium text-gray-900 block mb-1 mt-8"
+          >
+            Message
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+          />
+          <button className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 ">
+            Submit
+          </button>
+        </form>
+      </div>
+    </Section>
+  );
+};
